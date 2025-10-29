524b94b918fbfd4de8556c0a3286802f13c2bf85
y9san9 / Alex Sokol <y9san9@gmail.com>
2025-10-29 20:07:01 +0300

    Quartz sync: Oct 29, 2025, 8:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,30 +18,31 @@ const graph = Component.Graph({
     drag: true, // whether to allow panning the view around
     zoom: true, // whether to allow zooming in and out
     depth: 1, // how many hops of notes to display
-    scale: 0.9, // default view scale
-    repelForce: 2, // how much nodes should repel each other
+    scale: 1.1, // default view scale
+    repelForce: 0.5, // how much nodes should repel each other
     centerForce: 0.3, // how much force to use when trying to center the nodes
-    linkDistance: 60, // how long should the links be by default?
+    linkDistance: 30, // how long should the links be by default?
     fontSize: 0.6, // what size should the node labels be?
-    opacityScale: 3, // how quickly do we fade out the labels when zooming out?
+    opacityScale: 1, // how quickly do we fade out the labels when zooming out?
     removeTags: [], // what tags to remove from the graph
     showTags: true, // whether to show tags in the graph
+    enableRadial: false, // whether to constrain the graph, similar to Obsidian
   },
   globalGraph: {
     drag: true,
     zoom: true,
     depth: -1,
     scale: 0.9,
-    repelForce: 2,
+    repelForce: 0.5,
     centerForce: 0.3,
-    linkDistance: 60,
+    linkDistance: 30,
     fontSize: 0.6,
-    opacityScale: 3,
+    opacityScale: 1,
     removeTags: [], // what tags to remove from the graph
     showTags: true, // whether to show tags in the graph
+    enableRadial: true, // whether to constrain the graph, similar to Obsidian
   },
 })
-
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
