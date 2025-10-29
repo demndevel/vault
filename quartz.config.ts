b8ab3f713475616473c7fb714747decbce1a41a9
y9san9 / Alex Sokol <y9san9@gmail.com>
2025-10-29 19:07:12 +0300

    Quartz sync: Oct 29, 2025, 7:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,9 +22,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "JetBrains Mono",
-        body: "JetBrains Mono",
-        code: "JetBrains Mono",
+        header: "Iosevka",
+        body: "Iosevka",
+        code: "Iosevka",
       },
       colors: {
         lightMode: {
